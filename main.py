055b5a4e8c441e4b5e29e45a8d176229d6d0ca56
Tharusha-Delpachitra <tharushadelpa@gmail.com>
2025-03-01 20:13:06 +0530

    added the routes to get, add, delete and update user

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
-from uuid import uuid4
+from uuid import uuid4, UUID
 from fastapi import FastAPI
+
 from models import User, Gender, Role
 
 app = FastAPI()
@@ -22,11 +23,37 @@ db: List[User] = [
     )
 ]
 
+
 @app.get("/")
 async def root():
     return "API started successfully"
+
 
 @app.get("/users")
 async def get_users():
     return db
 
+
+@app.post("/users")
+async def create_user(user: User):
+    db.append(user)
+    return {"id": user.id}
+
+
+@app.delete("/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted successfully"}
+
+    return {"error": "User not found"}
+
+@app.put("/users/{user_id}")
+async def update_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.append(user)
+            return {"message": "User updated successfully"}
+
+    return {"error": "User not found"}
