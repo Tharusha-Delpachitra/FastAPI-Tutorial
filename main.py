c7928401e17b9c3518c5eece617140a8c5dc0804
Tharusha-Delpachitra <tharushadelpa@gmail.com>
2025-03-01 19:36:11 +0530

    Added the data and created a route to get the users

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+from models import User, Gender, Role
 
 app = FastAPI()
 
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="John",
+        last_name="Smith",
+        gender=Gender.male,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Rose",
+        last_name="Smith",
+        gender=Gender.female,
+        roles=[Role.admin, Role.user]
+    )
+]
+
 @app.get("/")
-def root():
-    return {"Hello": "world"}
+async def root():
+    return "API started successfully"
+
+@app.get("/users")
+async def get_users():
+    return db
 
